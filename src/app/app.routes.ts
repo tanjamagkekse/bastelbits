bed4493ca68310c36f80b0f19f0ed9539d85c74c
kWhjVQPT9sHGWaezqrUk <tanja.ulmen@posteo.de>
2024-02-23 11:03:49 +0100

    added basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+import { AboutComponent } from './about/about.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    
+    { path: 'skills', component: SkillsComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'timeline', component: TimelineComponent},
+
+
+    { path: '**', redirectTo: 'home', pathMatch: 'full' },
+  ];
