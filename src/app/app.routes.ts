ac705acfe22fb91d6cfb12eb8b628d11ec883d06
Tanja <tanja.ulmen@posteo.de>
2024-03-11 17:04:31 +0100

    added release-notes (incl routing and sidebar addition)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { SkillsComponent } from './skills/skills.component';
 import { AboutComponent } from './about/about.component';
 import { TimelineComponent } from './timeline/timeline.component';
 import { ArticleViewComponent } from './articles/article-view.component';
+import { ReleaseNotesComponent } from './release-notes/release-notes.component';
 
 export const routes: Routes = [
 
@@ -33,6 +34,10 @@ export const routes: Routes = [
       component: TimelineComponent
     },
 
+    { 
+      path: 'release-notes', 
+      component: ReleaseNotesComponent
+    },
     { 
       path: '**',
       redirectTo: 'home', 
