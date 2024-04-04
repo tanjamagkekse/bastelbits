7900adf1f323c84c077a57a9a6b2ac394638900a
Tanja <tanja.ulmen@posteo.de>
2024-04-04 11:27:20 +0200

    setting up a message service and a massage interface for further backend communication

---
+++ src/app/contact-form/message.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IMessage } from './message';
+
+@Injectable({
+    providedIn: 'root'
+  })
+
+export class MessageService{
+    private api = 'http://mailthis.to/alias'
+    messageData: IMessage = {name: '', email: '', message: ''}
+
+    constructor(private http: HttpClient){}
+
+    submitForm() {
+        const formData = {
+          email: this.messageData.email,
+          // Andere Formularfelder hier hinzufügen
+        };
+    
+        // Hier HTTP-Anfrage an deinen Backend-Service senden, der die E-Mail sendet
+        this.http.post('URL_DEINES_BACKEND_SERVICES', formData).subscribe(response => {
+          // Erfolgsnachricht anzeigen oder andere Aktionen ausführen
+        });
+      }
+}
